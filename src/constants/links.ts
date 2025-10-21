1e4f7083143f06e454bb8963e90f698e21578d4a
Delvia <119115171+viadel13@users.noreply.github.com>
2025-10-21 16:05:03 +0100

    Update links.ts

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,6 +1,6 @@
 export const pages = [
   { name: "Accueil", link: "/" },
-  { name: "Panier", link: "/paniers" },
+  { name: "Panier", link: "/panier" },
   { name: "Categories", link: "/categories" },
   { name: "A propos", link: "/about" },
   { name: "Contact", link: "/contact" },
